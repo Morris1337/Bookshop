a85212f54f4948b81261482cc48a1f3617a0b9ea
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2024-03-22 12:22:44 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import {scrollDown} from "./header.js";
 import {slider} from "./slider.js";
 import * as api from "./api.js";
 import {category} from "./mobile.js";
-// import "../style/style.scss";
+import "../style/style.scss";
 
 slider()
 category()
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
 let path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, "./js/main.js"),
+    entry: path.resolve(__dirname, "./src/js/main.js"),
     output: {
         path: path.resolve(__dirname, "output"),
         filename: "index.js"
     },
-    mode: "development",
+    mode: "production",
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        filename:"index.html",
+        template:"./index.html"
+      }),
+      new CopyWebpackPlugin({
+          patterns: [
+              { from: 'src/img', to: 'src/img' },
+              { from: 'src/font', to: "src/font"}
+          ],
+      }),
+    ],
     module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.scss$/i,
-            use: ["style-loader", "css-loader", "sass-loader"],
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         },
-        ],
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i, 
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(ttf)$/i, 
+          type: 'asset/resource',
+        }
+      ],
+    },
+    devServer:{
+      static:{
+          directory:path.join(__dirname, "./")
       },
+      port:5020
+    },
+    optimization: {
+      minimizer: [
+        "...",
+        new CssMinimizerPlugin(),
+      ],
+    },
 }
